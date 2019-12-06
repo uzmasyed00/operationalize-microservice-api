b0cdccb07c4c6007e53404ebd2d93a078c88e4b5
Uzma Syed <uzmasyed@192-168-1-10.tpgi.com.au>
2019-12-06 22:31:08 +1100

    changing title in app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home - changing title</h3>"
+    html = "<h3>Sklearn Prediction Home - Changing title</h3>"
     return html.format(format)
 
 @app.route("/predict", methods=['POST'])
